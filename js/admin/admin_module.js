fd28084807e9a58191228583fe7875edde6e74e1
Gabe Fletcher Sanchez <78129712+JuicyGaby@users.noreply.github.com>
2022-11-18 14:45:51 +0800

    Doc: Starting to create module

---
+++ js/admin/admin_module.js
@@ -0,0 +1,6 @@
+const addBtn = document.querySelector('#add-btn')
+const modulePopup = document.querySelector('#add-popup')
+
+addBtn.addEventListener('click', function() {
+    modulePopup.classList.toggle("active")
+})
