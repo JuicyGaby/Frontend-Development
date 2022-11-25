0cdfbad7615da8140b8efbe70a6beb9910451317
Gabe Fletcher Sanchez <78129712+JuicyGaby@users.noreply.github.com>
2022-11-25 22:11:18 +0800

    Docs : Created Module view v2

--- js/admin/admin_module.js
+++ js/admin/admin_module.js
@@ -1,11 +1,16 @@
-const addBtn = document.querySelector('#add-btn')
+const modulePopup = document.querySelector('#module-popup')
+const grayWrapper = document.querySelector('.hero-wrapper')
+const createModuleBtn = document.querySelector(".create-btn")
 const closeBtn = document.querySelector('#close-btn')
-const modulePopup = document.querySelector('#add-popup')
 
-addBtn.addEventListener('click', function() {
+
+
+createModuleBtn.addEventListener('click', function() {
     modulePopup.classList.add("active")
+    grayWrapper.classList.add("active")
 })
 closeBtn.addEventListener('click', function() {
     modulePopup.classList.remove("active")
+    grayWrapper.classList.remove("active")
 })
 
