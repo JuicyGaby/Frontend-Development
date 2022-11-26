12705ca0ba0e55378f7127613017b72b01cd224c
Gabe Fletcher Sanchez <78129712+JuicyGaby@users.noreply.github.com>
2022-11-26 16:37:33 +0800

    Created the update for feed and module

--- js/admin/admin_module.js
+++ js/admin/admin_module.js
@@ -1,8 +1,13 @@
-const modulePopup = document.querySelector('#module-popup')
 const grayWrapper = document.querySelector('.hero-wrapper')
+const modulePopup = document.querySelector('#module-popup')
 const createModuleBtn = document.querySelector(".create-btn")
 const closeBtn = document.querySelector('#close-btn')
 
+
+// filter 
+const filterPopup = document.querySelector('#filter-popup')
+const filterBtn = document.querySelector('.filter-btn')
+const filterCloseBtn = document.querySelector('#filter-close-btn')
 
 
 createModuleBtn.addEventListener('click', function() {
@@ -14,3 +19,13 @@ closeBtn.addEventListener('click', function() {
     grayWrapper.classList.remove("active")
 })
 
+
+filterBtn.addEventListener('click', function() {
+    filterPopup.classList.add("active")
+    grayWrapper.classList.add("active")
+
+})
+filterCloseBtn.addEventListener('click', function() {
+    filterPopup.classList.remove("active")
+    grayWrapper.classList.remove("active")
+})
